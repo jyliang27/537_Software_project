ae6eb0dd91b78fd2f41e5b3dbd5501caf94ba954
Jia <jyliang@uw.edu>
2024-12-03 03:19:07 -0800

    Added function documentation

--- src/program.py
+++ src/program.py
@@ -2,16 +2,37 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-def LoadData(filename:str):
-    if ".csv" in filename:
-        dataframe=pd.read_excel(str(filename))
-    if ".xlsx" in filename:
-        dataframe=pd.read_excel(str(filename)) 
+def LoadData(filepath:str):
+    """
+    LoadData takes in a filename and loads the data into a dataframe. Only Excel and CSV files are accepted; other formats will return an error message.
+    
+    Input:
+    filepath: the path of the file containing experimental data. Include the file extension. Please note, data should be formatted with time and concentration in columns; all datapoints in a column should have the same units.
+
+    Output:
+    Returns a dataframe containing raw experimental data. Please assign a name for this dataframe outside of the function.
+    """
+    if ".csv" in filepath:
+        dataframe=pd.read_excel(str(filepath))
+    if ".xlsx" in filepath:
+        dataframe=pd.read_excel(str(filepath)) 
     else:
         return print("File in wrong format") 
     return dataframe
 
-def PrepData(dataframe:str,concentration:str,lnConc:str) :
+def PrepData(dataframe:str,concentration:str,lnConc:str):
+    """
+    PrepData makes a copy of a dataframe, log transforms data from a column of interest, removes any rows with null values in the log transformed data, and returns a new dataframe
+    containing the original data as well as a new column with log transformed data.
+
+    Inputs:
+    dataframe: the name assigned to the dataframe containing you
+    concentration: the name of the dataframe column containing concentration data
+    lnConc: the desired name of a dataframe column containing log transformed concentration data
+
+    Output:
+    A new dataframe containing the original data containing the original data as well as a new column with log transformed data. Please assign a name to this dataframe outside of the function.
+    """
     tempdf=dataframe.copy()
     ln_list=[]
     for i in dataframe.index:
@@ -24,6 +45,17 @@ def PrepData(dataframe:str,concentration:str,lnConc:str) :
     return tempdf
 
 def findCmax(dataframe:str,time:str, concentration:str)->float:
+    """
+    This function takes in a dataframe and columns of interest, identifies the highest drug concentration in the data, and then identifies the first timepoint at which that drug concentration is measured.
+
+    Inputs:
+    dataframe: name of the dataframe
+    time: name of dataframe column containing time data
+    concentration: name of dataframe column containing concentration data
+
+    Outputs:
+    Returns a 1x2 array containing the values for Cmax and Tmax.
+    """
     for i in dataframe.index:
         if dataframe[concentration][i]==np.nanmax(dataframe[concentration]):
             if i==0:
@@ -39,7 +71,20 @@ def findCmax(dataframe:str,time:str, concentration:str)->float:
     print("Tmax=",Tmax)
     return Cmax, Tmax
 
-def findT_half(dataframe, time, concentration, percent_threshold)->float:
+def findT_half(dataframe:str, time:str, concentration:str, percent_threshold:float)->float:
+    """
+    This takes in a dataframe, columns of interest, and a threshold expressed as a percent. Scans the data to find timepoints at which drug concentration has decreased by roughly 50%.
+    Note: Since data might be sparse, drug concentration at the latter time point can fall within the percent_threshold of the target value.
+
+    Inputs:
+    dataframe: name of the dataframe
+    time: name of dataframe column containing time data
+    concentration: name of dataframe column containing concentration data
+    percent_threshold: a percent (sign not needed) representing percent. Ex: a value of 10 should be interpreted as within 10% of target value
+
+    Outputs:
+    A float that represents the time it takes for drug concentration to decrease by roughly 50%. Units are the same as the units of the time data.
+    """
     t_half="Not Assigned, consider adjusting threshold"
     for i in dataframe.index:
         if dataframe[concentration][i]==np.nanmax(dataframe[concentration]):
@@ -55,21 +100,23 @@ def findT_half(dataframe, time, concentration, percent_threshold)->float:
                     break
     return t_half
 
-def findCo(dataframe:str,time:str,lnConc:str) -> float:
+def findCo(dataframe:str,time:str,lnConc:str, model:str="1 compartment, IV") -> float:
     """
-    This function takes in a dataframe with a column for time and a column with ln(drug concentration)
-    and returns the elimination constant k, which is equivalent to -1 * the coefficient (or slope);
-    and the drug concentration at t=0, which is equivalent to the y intercept.
+    This function takes in a dataframe with a column for time and a column with ln(drug concentration), fits a PK model, and returns the elimination constant k and the drug concentration at t=0.
+    This function is currently only built for a 1 compartment model for IV bolus drug administration.
     
-    This function currently assumes data is linear.
-    
+    Input:
     dataframe: the name of the dataframe
-    time: the name of time column
-    lnConc: the name of the column with ln(drug concentration)
+    time: the name of dataframe column containing time data
+    lnConc: the name of the dataframe column with ln(drug concentration)
+
+    Output:
+    Returns a 1x2 array with values for Co and k.
     """
     x = []
     y = []
-    model = LinearRegression()
+    if model == "1 compartment, IV":
+        testmodel = LinearRegression()
     for i in dataframe.index:
         tempvalue = dataframe[time][i]
         templist = []
@@ -79,14 +126,28 @@ def findCo(dataframe:str,time:str,lnConc:str) -> float:
         templist2 = []
         templist2.append(tempvalue2)
         y.append(templist2)
-    model.fit(x,y)
-    lnCo = float(model.intercept_)
+    testmodel.fit(x,y)
+    lnCo = float(testmodel.intercept_)
     Co = np.exp(lnCo)
-    k = float(-1*model.coef_)
+    k = float(-1*testmodel.coef_)
     return Co,k
 
-def findPK(dataframe:str,time:str, concentration:str,lnConc:str, percent_threshold:float)->float:
-    result1 = findCo(dataframe, time, lnConc)
+def findPK(dataframe:str,time:str, concentration:str,lnConc:str, percent_threshold:float, model:str="1 compartment, IV")->float:
+    """
+    This function takes in a dataframe, the names of any columns of interest, a percent threshold, and the desired PK model to be fit. It passes these arguments to the functions findCo, findCmax, and findT_half, and returns a 1x5 array with the collected function outputs.
+
+    Inputs:
+    dataframe: the name of the dataframe
+    time: the name of time column
+    concentration
+    lnConc: the name of the column with ln(drug concentration)
+    percent_threshold
+    model: keyword for the PK model to be applied.
+
+    Outputs:
+    Returns a 1x5 array with values for Co, k, Cmax, Tmax, t_half
+    """
+    result1 = findCo(dataframe, time, lnConc, model)
     result2 = findCmax(dataframe,time, concentration)
     result3 = findT_half(dataframe, time, concentration, percent_threshold)
     k = result1[1]
