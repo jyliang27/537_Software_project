f30f163fb1041ac96e7ad58b7c3200fe2f83a7e9
Jia <jyliang@uw.edu>
2024-12-09 20:05:02 -0800

    updated init

--- ModelPK/__init__.py
+++ ModelPK/__init__.py
@@ -2,7 +2,7 @@ from .extractPKparam import *
 from .simulatePK import *
 
 __version__ = '0.1.0'
-__author__ = "Susan Ling"
-__email__ = "srling@uw.edu"
+__author__ = "Jia Liang"
+__email__ = "jyliang@uw.edu"
 
 __all__ = ["extractPK","simulatePK"]
